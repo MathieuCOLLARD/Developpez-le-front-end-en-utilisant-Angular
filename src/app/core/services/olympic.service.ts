66fc1d211d83d0e984811db18564bc07121c37a6
MathieuCOLLARD <108083456+MathieuCOLLARD@users.noreply.github.com>
2023-10-20 18:18:51 +0200

    imporve error handling

--- src/app/core/services/olympic.service.ts
+++ src/app/core/services/olympic.service.ts
@@ -2,6 +2,7 @@ import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { BehaviorSubject } from 'rxjs';
 import { catchError, tap } from 'rxjs/operators';
+import { HttpErrorResponse } from '@angular/common/http';
 
 @Injectable({
   providedIn: 'root',
@@ -16,10 +17,18 @@ export class OlympicService {
     return this.http.get<any>(this.olympicUrl).pipe(
       tap((value) => this.olympics$.next(value)),
       catchError((error, caught) => {
-        // TODO: improve error handling
         console.error(error);
+        
         // can be useful to end loading state and let the user know something went wrong
-        this.olympics$.next(null);
+        if (error instanceof HttpErrorResponse) {
+          if (error.status === 0) {
+            this.olympics$.next({ error: 'Network error. Please check your internet connection.' });
+          } else {
+            this.olympics$.next({ error: 'Server error. Please try again later.' });
+          }
+        } else {
+          this.olympics$.next({ error: 'An error occurred while loading data.' });
+        }
         return caught;
       })
     );
